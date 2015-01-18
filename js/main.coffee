d366690818e1c883c3ca6112b2d8951fd3dddc0c
Joshua Ellington <hello@joshellington.com>
2015-01-17 22:05:55 -0800

    Switched to Mike's splash idea

--- js/main.coffee
+++ js/main.coffee
@@ -1,6 +1,20 @@
 
 $ ->
-  # setHeight('.poster, .poster img')
+  if window.innerHeight > 600
+    setHeight('header')
+    setMargin('.wrapper')
+
+    $(window).on 'resize', =>
+      setHeight('header')
+
+  $(window).load ->
+    loaded()
 
 setHeight = (elements) ->
   $(elements).height(window.innerHeight)
+
+setMargin = (elements) ->
+  $(elements).css 'margin-top', window.innerHeight
+
+loaded = ->
+  $('body').addClass('loaded')
--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 (function() {
-  var setHeight;
+  var loaded, setHeight, setMargin;
 
-  $(function() {});
+  $(function() {
+    if (window.innerHeight > 600) {
+      setHeight('header');
+      setMargin('.wrapper');
+      $(window).on('resize', (function(_this) {
+        return function() {
+          return setHeight('header');
+        };
+      })(this));
+    }
+    return $(window).load(function() {
+      return loaded();
+    });
+  });
 
   setHeight = function(elements) {
     return $(elements).height(window.innerHeight);
+  };
+
+  setMargin = function(elements) {
+    return $(elements).css('margin-top', window.innerHeight);
+  };
+
+  loaded = function() {
+    return $('body').addClass('loaded');
   };
 
 }).call(this);
